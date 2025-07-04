dd8725556a98bc7e1ce987630c0c9167bcf9faee
Camilo Santiago <zansanti93@gmail.com>
2025-07-04 19:27:18 -0300

    teste

--- src/controllers/carriersController.ts
+++ src/controllers/carriersController.ts
@@ -3,10 +3,12 @@ import * as carrierRepository from "../repositories/carrierRepository";
 
 export async function getCarriers(req: Request, res: Response) {
   try {
-    const carriers = await carrierRepository.getCarriers();
+   // const carriers = await carrierRepository.getCarriers(); 
+   const carriers = [ "Camilo 1, 2, 3" ]
     res.status(200).json(carriers);
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
   }
 }
+
